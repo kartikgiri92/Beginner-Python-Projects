fadc2416295211d0e74af18c7f2470d11b3db2a2
kartikgiri <kartikgiri92@gmail.com>
2018-09-25 22:31:45 +0530

    removed DjangoPRoject

--- roughdjango/quiz/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'quiz'
-urlpatterns = [
-	path('', views.HomePageView.as_view(), name = 'HomePage'),
-	path('<int:pk>/', views.QuizPageView.as_view(), name = 'QuizPage'),
-]
